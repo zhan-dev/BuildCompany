1a416e17538d0fc3b4d09930689154b41a3af5d7
Zhan <Zhan.Kariahin@gmail.com>
2025-06-16 16:30:14 +0300

    update

---
+++ BuildCompany/Controllers/Admin/Services.cs
@@ -0,0 +1,45 @@
+﻿using BuildCompany.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildCompany.Controllers.Admin
+{
+    public partial class AdminController
+    {
+        public async Task<IActionResult> ServicesEdit(int id)
+        {
+            Service? entity = id == default ? new Service() : await _dataManager.Services.GetServiceByIdAsync(id);
+            ViewBag.ServiceCategories = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServicesEdit(Service entity, IFormFile? titleImageFile)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ServiceCategories = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+
+                return View(entity);
+
+            }
+            if (titleImageFile != null)
+            {
+                entity.Photo = titleImageFile.FileName;
+                await SaveImg(titleImageFile);
+            }
+
+            await _dataManager.Services.SaveServiceAsync(entity);
+
+            return RedirectToAction("Admin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServicesDelete(int id)
+        {
+            await _dataManager.Services.DeleteServiceAsync(id);
+
+            return RedirectToAction("Admin");
+        }
+    }
+}
---
+++ BuildCompany/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildCompany.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string? UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [UIHint("password")]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
